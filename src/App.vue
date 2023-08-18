f19984c03923cd0d42925eb8ca7db2772aa8d7f1
Morgan Peck <mlpeck@arkbluecross.com>
2023-08-18 09:01:57 -0500

    starting the countdown stuff

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Countdown from './components/Countdown.vue'
+</script>
+
+<template>
+  <main>
+    <Countdown />
+  </main>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Alumni+Sans+Inline+One&family=Roboto+Condensed:wght@300;400;700&display=swap');
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: block;
+}
+
+main {
+  height: 100%;
+
+  font-family: 'Roboto Condensed', sans-serif;
+}
+</style>
---
+++ src/components/Countdown.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="inner">
+    <div class="countdown-wrapper">
+      <p>
+        <span class="time">{{ days }}</span> <span class="label">Day</span>
+      </p>
+
+      <p>
+        <span class="time">{{ hours }}</span> <span class="label">Hours</span>
+      </p>
+
+      <p>
+        <span class="time">{{ mins }}</span
+        ><span class="label">Minutes</span>
+      </p>
+
+      <p>
+        <span class="time">{{ secs }}</span> <span class="label">Seconds</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    days: 0,
+    hours: 0,
+    mins: 0,
+    secs: 0
+  })
+}
+</script>
+
+<style scoped lang="scss">
+.inner {
+  widows: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 0 auto;
+}
+
+.countdown-wrapper {
+  display: flex;
+  align-items: center;
+  gap: 4rem;
+
+  padding: 2rem;
+
+  p {
+    display: grid;
+    grid-template-rows: 5rem 1fr;
+    grid-template-columns: 100%;
+
+    justify-content: center;
+    align-items: center;
+
+    .label {
+      font-size: 2rem;
+    }
+
+    &:last-of-type .time::after {
+      display: none;
+    }
+  }
+}
+
+.time {
+  height: 5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  grid-row: 1;
+
+  font-family: 'Alumni Sans Inline One', cursive;
+  font-size: 8rem;
+
+  &::after {
+    content: ':';
+
+    position: absolute;
+    right: -2.5rem;
+
+    font-family: 'Roboto Condensed', sans-serif;
+    font-size: 4rem;
+  }
+}
+</style>
